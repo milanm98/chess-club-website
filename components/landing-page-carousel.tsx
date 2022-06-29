9dfc1edd601b931a3f6a099e19e10408d6ff5c77
milan.marjanov <milan.marjanov@pbssolutions.net>
2022-06-29 19:53:38 +0200

    Add real images to slider instead of dummy data

--- components/landing-page-carousel.tsx
+++ components/landing-page-carousel.tsx
@@ -2,15 +2,16 @@ import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import SliderImages from './sliderImages.json'
+import { Container } from '@material-ui/core'
 
 const settings = {
   dots: true,
   infinite: true,
-  speed: 500,
+  speed: 200,
   slidesToShow: 1,
   slidesToScroll: 1,
   autoplay: true,
-  autoplaySpeed: 1500,
+  autoplaySpeed: 2500,
   adaptiveHeight: true,
   className: 'w-full',
   arrows: false,
@@ -18,19 +19,20 @@ const settings = {
 
 const LandingPageCarousel = () => {
   return (
-    <Slider {...settings}>
-      {SliderImages.map((image) => {
-        return (
-          // eslint-disable-next-line @next/next/no-img-element
-          <img
-            className="h-[400px] lg:h-[700px]"
-            src={image.src}
-            key={image.key}
-            alt="nesto"
-          />
-        )
-      })}
-    </Slider>
+    <Container maxWidth="lg">
+      <Slider {...settings}>
+        {SliderImages.map((image) => {
+          return (
+            <img
+              className="h-[400px] lg:h-[700px]"
+              src={image.src}
+              key={image.key}
+              alt="nesto"
+            />
+          )
+        })}
+      </Slider>
+    </Container>
   )
 }
 

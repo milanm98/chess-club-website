8cc4194ae75bffa665a1146eb15c44e9219422c2
milan-marjanov <milan.marjanov@pbssolutions.net>
2023-01-03 14:11:12 +0100

    add 6th post

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -24,8 +24,8 @@ const Navbar = () => {
         </div>
 
         <ul
-          className={` md:flex md:items-center md:pb-0 pb-12 absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9  shadow-lg md:shadow-none transition-all duration-500 ease-in ${
-            open ? 'top-20' : 'top-[-490px]'
+          className={` md:flex md:items-center nav-background-color md:pb-0 pb-12 absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9  shadow-lg md:shadow-none transition-all duration-500 ease-in ${
+            open ? 'top-1' : 'top-[-490px]'
           }`}
         >
           {Links.map((link) => (

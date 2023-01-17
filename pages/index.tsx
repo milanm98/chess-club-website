ad5983d8a9abf9a83d57aa53e4f7ef7abfb83267
milan-marjanov <milan.marjanov@pbssolutions.net>
2023-01-17 23:28:01 +0100

    implement seo component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import type { NextPage } from 'next'
 import LandingPageCarousel from '../components/landing-page-carousel'
 import Posts from '../components/posts'
@@ -7,24 +6,13 @@ import Header from '../components/header'
 import AboutUs from '../components/about-us'
 import Contact from '../components/contact'
 import Tournaments from '../components/tournaments'
+import Seo from '../components/seo'
 
 const Home: NextPage = () => {
   return (
     <main className="app-background-color">
-      <Head>
-        <title>Š.K. Gimnazijalac</title>
-        <meta
-          name="description"
-          content="Sahovski klub Gimnazijalac Backa Palanaka"
-        />
-        <meta name="keywords" content="sah klub, gimnazijalac" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1"
-        ></meta>
-        <meta charSet="utf-8"></meta>
-      </Head>
       <Layout>
+        <Seo />
         <LandingPageCarousel />
         <Header />
         <Posts />

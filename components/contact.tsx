ec94c321c92e1681ddb1041fa705189d789ea756
milan-marjanov <milan.marjanov@pbssolutions.net>
2023-02-15 13:42:15 +0100

    refactore code and add new posts

--- components/contact.tsx
+++ components/contact.tsx
@@ -7,43 +7,43 @@ const Contact = () => {
       <div className="flex flex-col md:flex-row my-16 gap-4 items-center justify-center md:flex-wrap">
         <div className="max-w-sm rounded overflow-hidden shadow-2xl relative">
           <div className="px-6 py-4 min-h-[300px] min-w-[300px] card-bg-color">
-            <h2 className="font-bold mb-6">Rukovodstvo:</h2>
-            <h3 className="font-bold mb-2">Predsednik:</h3>
-            <h3 className="mb-6">Ivan Tot - 0692705990</h3>
-            <h3 className="font-bold">Sekretar:</h3>
-            <h3>Martin Hlozan - 062558083</h3>
+            <h3 className="font-bold mb-6">Rukovodstvo:</h3>
+            <h4 className="font-bold mb-2">Predsednik:</h4>
+            <h4 className="mb-6">Ivan Tot - 0692705990</h4>
+            <h4 className="font-bold">Sekretar:</h4>
+            <h4>Martin Hlozan - 062558083</h4>
           </div>
         </div>
 
         <div className="max-w-sm rounded overflow-hidden shadow-2xl relative">
           <div className="px-6 py-4 min-h-[300px] min-w-[300px] card-bg-color">
-            <h2 className="font-bold mb-6">Email adrese:</h2>
-            <h3 className="font-bold mb-2">Sahovski klub Gimnazijalac:</h3>
-            <h3 className="mb-6"> gimnazijalacinfo@gmail.com</h3>
-            <h3 className="font-bold">Gimnazija:</h3>
-            <h3>info@gimnazijabp.edu.rs</h3>
+            <h3 className="font-bold mb-6">Email adrese:</h3>
+            <h4 className="font-bold mb-2">Sahovski klub Gimnazijalac:</h4>
+            <h4 className="mb-6"> gimnazijalacinfo@gmail.com</h4>
+            <h4 className="font-bold">Gimnazija:</h4>
+            <h4>info@gimnazijabp.edu.rs</h4>
           </div>
         </div>
 
         <div className="max-w-sm rounded overflow-hidden shadow-2xl relative">
           <div className="px-6 py-4 min-h-[300px] min-w-[300px] card-bg-color">
-            <h2 className="font-bold mb-6">Linkovi:</h2>
+            <h3 className="font-bold mb-6">Linkovi:</h3>
             <a
-              className="font-bold text-xl mb-8 text-color underline hover:text-zinc-600 inline-block"
+              className="font-bold mb-8 text-color underline hover:text-zinc-600 inline-block"
               href="https://lichess.org/team/gimnazijalac"
               target="_blank"
               rel="noreferrer"
             >
-              Lichess
+              <h4>Lichess</h4>
             </a>
             <br />
             <a
-              className="font-bold text-xl mb-8 text-color underline  hover:text-zinc-600 inline-block"
+              className="font-bold mb-8 text-color underline  hover:text-zinc-600 inline-block"
               href="https://www.facebook.com/%C5%A0ah-klub-Gimnazijalac-979024278831201"
               target="_blank"
               rel="noreferrer"
             >
-              Facebook
+              <h4>Facebook</h4>
             </a>
           </div>
         </div>
--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Barlow&display=swap"
           rel="stylesheet"
         />
       </Head>
